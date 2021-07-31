73be0cbe6b9c8fe7c0850e9eaeacb27e454e122b
umutsevdi <sevdiumut@gmail.com>
2021-07-31 11:00:32 +0300

    Left Bar 2.0

--- ui/JSON View.gd
+++ ui/JSON View.gd
@@ -1,8 +1,14 @@
 extends TextEdit
 
 func _ready():
-	self.add_keyword_color("head",Color("#b1c378"))
-	self.add_keyword_color("import",Color("#af0ac8"))
-	self.add_keyword_color("table",Color.thistle)
+	self.add_keyword_color("header",Color("#afaa63"))
+	self.add_keyword_color("import",Color("#afaa63"))
+	self.add_keyword_color("table",Color("afaa63"))
 	self.add_keyword_color("connect",Color("#afaa63"))
+	self.add_color_region("\"","\"",Color("#70d56d"))
 	
+	var elements := [
+		"name","type","input","output","source","path"
+	]
+	for i in elements:
+		self.add_keyword_color(i,Color("#af0ac8"))
